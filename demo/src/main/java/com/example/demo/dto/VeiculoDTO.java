a877e0f0927d3471e12329c431dbca73cf168e45
Bruna Carla <luisfepsale@hotmail.com>
2020-12-02 10:51:23 -0300

    DTO

--- demo/src/main/java/com/example/demo/dto/VeiculoDTO.java
+++ demo/src/main/java/com/example/demo/dto/VeiculoDTO.java
@@ -1,21 +1,22 @@
 package com.example.demo.dto;
 
-
 public class VeiculoDTO {
+
 
     private String modelo;
     private float valorDiaria;
 
-    public String getModelo() {
+
+    public String getModelo(){
         return modelo;
     }
-    public void setModelo(String modelo) {
+    public void setModelo(String modelo){
         this.modelo = modelo;
     }
-    public float getValorDiaria() {
+    public float getValorDiaria(){
         return valorDiaria;
     }
-    public void setValorDiaria(float valorDiaria) {
+    public void setValorDiaria(float valorDiaria){
         this.valorDiaria = valorDiaria;
     }
 }
